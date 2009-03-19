ad4707311e66ff66b6eb618bf5a2625ffee849f5
eric-n <eric-n@is.naist.jp>
2009-03-19 16:13:42 +0900

    Merge from pine.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 ## Add system-specific environment definitions.
 ## Assumes MHOME is pointing to this directory.
+## Add the following to your .bashrc (or .zshrc):
+
+## export MHOME=/path/to/mosestools
+## if [ -f $MHOME/setup.sh ]; then
+## 	. $MHOME/setup.sh
+## fi
+
 
 export PATH=$MHOME/bin:$PATH
 
